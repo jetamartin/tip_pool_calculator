391e455975d51c3e6f64203dc0df943a2e2cbded
Jet Martin <jetamartin@gmail.com>
2020-01-26 13:49:03 -0800

    completed jasmine test cases for app.js file

--- javascript/app.test.js
+++ javascript/app.test.js
@@ -25,54 +25,95 @@ describe("Delete Server and Payment data via table row delete button", function(
 
   beforeEach (function() {
     serverNameInput.value = 'Alice';
+    submitServerInfo();
+
+    billAmtInput.value = 100;
+    tipAmtInput.value = 10;
     submitPaymentInfo();
 
     
   });
 
   it ("it should remove the server's row from the UI's Server table when the row's delete button is clicked", function () {
-   
-    let deleteBtn = document.querySelector("td > button");
+    submitPaymentInfo();
+    let deleteBtn = document.querySelector("#serverTable td > button");
     
     deleteTableRow(deleteBtn);
     // No servers objects in allServers object
     expect(Object.keys(allServers).length).toEqual(0);
+    expect(document.querySelector("#serverTable tbody").firstElementChild).toEqual(null);
+   
+  });
+
+  it ("it should remove a server & update remaining servers' earnings in UI's server table to reflect deletion of a server", function(){
+    serverNameInput.value = 'Rick';
+    submitServerInfo();
+
+    let deleteBtn = document.querySelector("#serverTable td > button");
+    deleteTableRow(deleteBtn);
+    expect(Object.keys(allServers).length).toEqual(1);
     
-    // No servers displayed on server
-    // - Server's table has zero tr's in tbody of #id=serverTable
-  
+    expect(document.querySelector("#serverTable tbody tr td").nextElementSibling.innerText).toEqual("$10.00");
+    
   });
 
-  it ("it should update remaining servers' earnings in UI's server table to reflect deletion of a server", function(){
+  // // Deletion of Payment information 
 
-
-  });
-
-  it ("it should update the allServers object to reflect the server's deletion from the UI", function () {
-
-  });
-
-  // Deletion of Payment information 
-
-  it ("it should remove a row of data from UI Payment table when that row's delete buttion is clicked", function () {
-
-  });
-
-  it ("it should update the allPayments object to reflect the server data deleted from the UI", function () {
-
+  it ("it should remove payment data from the UI's Payment table and update the allPayments object when that row's delete buttion is clicked", function () {
+    
+    let deleteBtn = document.querySelector("#paymentTable td > button");
+    
+    deleteTableRow(deleteBtn);
+    // No servers objects in allServers object
+    expect(Object.keys(allPayments).length).toEqual(0);
+    expect(document.querySelector("#paymentTable tbody").firstElementChild).toEqual(null);
   });
 
   it ("it should update the UI's Shift Summary table to reflect deletion of data from the UI Payment table ", function () {
+    billAmtInput.value = 200;
+    tipAmtInput.value = 20;
+    submitPaymentInfo();
 
-  });
+    let deleteBtn = document.querySelector("#paymentTable td > button");
+    
+    deleteTableRow(deleteBtn);
+
+    let summaryTableTr = document.querySelector("#summaryTable tbody tr");
+    expect(summaryTableTr.firstElementChild.innerText).toEqual("$200");
+    expect(summaryTableTr.firstElementChild.nextElementSibling.innerText).toEqual("$20");
+    expect(summaryTableTr.lastElementChild.innerText).toEqual("10%");
+   });
 
   it ("it should update the Earnings amounts of the Servers in the Server table when a Payment is deleted", function () {
+    serverNameInput.value = 'Rick';
+    submitServerInfo();
+
+    billAmtInput.value = 200;
+    tipAmtInput.value = 20;
+    submitPaymentInfo();
+
+    let deleteBtn = document.querySelector("#paymentTable td > button"); 
+    deleteTableRow(deleteBtn);
+
+    expect(document.querySelector("#serverTable tbody tr").firstElementChild.nextElementSibling.innerText).toEqual("$10.00");
 
   });
 
   afterEach (function() {
     serverTbody.innerHTML = "";
     allServers = {};
+
+    allPayments = {}
+    billAmtInput.value = "";
+    tipAmtInput.value = "";
+    // if a prior test case deleted the only payment then no need to clear out payment table entry or summary items
+    if (document.querySelector("#paymentTable tbody tr")) {
+      document.querySelector("#paymentTable tbody tr").remove();
+      document.querySelector("#summaryTable tbody tr").firstElementChild.innerText = "";
+      document.querySelector("#summaryTable tbody tr").lastElementChild.innerText = "";
+      document.querySelector("#summaryTable tbody tr").lastElementChild.previousElementSibling.innerText = "";
+    }
+   
   });
 
 }); 
